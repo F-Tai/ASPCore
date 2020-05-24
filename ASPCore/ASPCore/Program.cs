eda4832ece6522874e1429cc199fbdf5f36fb4f6
NN <NN@PC11>
2020-05-24 08:09:21 +0700

    init

--- ASPCore/ASPCore/Program.cs
+++ ASPCore/ASPCore/Program.cs
@@ -21,6 +21,6 @@ namespace ASPCore
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }); 
     }
 }
