c09b1d7f20aba63987c92a61188c2549c3a6872a
NN <kidokawr@gmail.com>
2020-05-31 11:50:33 +0700

    commit

---
+++ ASPCore/ASPCore/Controllers/HocVienController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ASPCore.Controllers
+{
+    public class HocVienController : Controller
+    {
+        public IActionResult DemoJson()
+        {
+            var hv = new HocVien
+            { MaSo = 1, TenHocVien = "Json", SoDienThoai = "123456", DiemTrungBinh = 3, GioiTinh = true };
+
+            var data = new
+            {
+                HocVien = hv,
+                TrungTam = "Nhất Nghệ",
+                NgayThanhLap = new DateTime(1990, 10, 10)
+            };
+            return Json(data);
+        }
+        [HttpGet]
+        public IActionResult ThongTin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ThongTin(HocVien hv, string Ghi)
+        {
+            if (Ghi == "Ghi File Text")
+            {
+                StreamWriter sw = new StreamWriter("HocVien.txt");
+                sw.WriteLine(hv.MaSo);
+                sw.WriteLine(hv.TenHocVien);
+                sw.WriteLine(hv.SoDienThoai);
+                sw.WriteLine(hv.DiemTrungBinh);
+                sw.WriteLine(hv.GioiTinh);
+                sw.Close();
+            }
+            else if (Ghi == "Ghi File JSON")
+            {
+                string ChuoiJson = JsonConvert.SerializeObject(hv);
+                System.IO.File.WriteAllText("HocVien.json", ChuoiJson);
+            }
+            return View();
+        }
+
+        public IActionResult DocFileJSON()
+        {
+            var content = System.IO.File.ReadAllText("HocVien.json");
+            var hocvien = JsonConvert.DeserializeObject<HocVien>(content);
+            return View("ThongTin",hocvien);
+        }
+
+        public IActionResult DocFileText()
+        {
+            var content = System.IO.File.ReadAllLines("HocVien.txt");
+            var hocvien = new HocVien
+            {
+                MaSo = int.Parse(content[0]),
+                TenHocVien = content[1],
+                SoDienThoai = content[2],
+                DiemTrungBinh = double.Parse(content[3]),
+                GioiTinh = content[4] == "true" ? true : false
+            };
+            return View("ThongTin", hocvien);
+        }
+    }
+}
---
+++ ASPCore/ASPCore/Models/HocVien.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore.Models
+{
+    public class HocVien
+    {
+        [DisplayName("Mã Học Viên")]
+        public int MaSo { get; set; }
+        [DisplayName("Tên Học Viên")]
+        public string TenHocVien { get; set; }
+        [DisplayName("Số Điện Thoại")]
+        public string SoDienThoai { get; set; }
+        [DisplayName("Điểm Trung Bình")]
+        public double DiemTrungBinh { get; set; }
+        [DisplayName("Giới Tính")]
+        public bool GioiTinh { get; set; }
+    }
+}
