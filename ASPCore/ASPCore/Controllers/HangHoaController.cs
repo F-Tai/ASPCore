2788ffc9cb2b22b5ebf22b08d673bb91b115d54c
NN <kidokawr@gmail.com>
2020-05-31 10:50:11 +0700

    commit

---
+++ ASPCore/ASPCore/Controllers/HangHoaController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using ASPCore.Models;
+
+namespace Buoi07.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        static List<HangHoa> dsHangHoa = new List<HangHoa>();
+
+        public IActionResult Index()
+        {
+            return View(dsHangHoa);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoa hh, IFormFile Hinh)
+        {
+            if (Hinh != null)
+            {
+                var filename = $"{DateTime.Now.Ticks}_{Hinh.FileName}";
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "hanghoa", filename);
+                hh.Hinh = filename;
+                using (var file = new FileStream(fullPath, FileMode.Create))
+                {
+                    Hinh.CopyTo(file);
+                    dsHangHoa.Add(hh);
+                    return Redirect("/HangHoa");
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ ASPCore/ASPCore/Models/HangHoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore.Models
+{
+    public class HangHoa
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double Dongia { get; set; }
+        public int Giamgia { get; set; }
+        public double Giaban => Dongia * (100 - Giamgia) / 100.0;
+
+    }
+}
