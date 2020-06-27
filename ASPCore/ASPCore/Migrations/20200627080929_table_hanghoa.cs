a602fefa0d08930bd571ce0d8d793582bdb325ae
NN <kidokawr@gmail.com>
2020-06-27 16:41:21 +0700

    Commit all

---
+++ ASPCore/ASPCore/Migrations/20200627080929_table_hanghoa.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASPCore.Migrations
+{
+    public partial class table_hanghoa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Loais",
+                table: "Loais");
+
+            migrationBuilder.RenameTable(
+                name: "Loais",
+                newName: "Loai");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Loai",
+                table: "Loai",
+                column: "MaLoai");
+
+            migrationBuilder.CreateTable(
+                name: "HangHoa",
+                columns: table => new
+                {
+                    MaHH = table.Column<Guid>(nullable: false),
+                    TenHH = table.Column<string>(maxLength: 50, nullable: false),
+                    DonGia = table.Column<double>(nullable: false),
+                    SoLuong = table.Column<int>(nullable: false),
+                    Hinh = table.Column<string>(nullable: true),
+                    MaLoai = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HangHoa", x => x.MaHH);
+                    table.ForeignKey(
+                        name: "FK_HangHoa_Loai_MaLoai",
+                        column: x => x.MaLoai,
+                        principalTable: "Loai",
+                        principalColumn: "MaLoai",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HangHoa_MaLoai",
+                table: "HangHoa",
+                column: "MaLoai");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HangHoa");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Loai",
+                table: "Loai");
+
+            migrationBuilder.RenameTable(
+                name: "Loai",
+                newName: "Loais");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Loais",
+                table: "Loais",
+                column: "MaLoai");
+        }
+    }
+}
---
+++ ASPCore/ASPCore/Models/MyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore.Models
+{
+    /// <summary>
+    /// Chuoi ket noi
+    /// </summary>
+    // https://www.connectionstrings.com/sql-server
+    //
+    public class MyDbContext : DbContext
+    {
+        //Dinh nghia cac thuoc tinh table
+        public DbSet<Loai> Loais { get; set; }
+        public DbSet<HangHoa>HangHoas { get; set; }
+
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
